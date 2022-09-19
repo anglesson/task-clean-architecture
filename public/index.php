7e52165671504a0cc1acc779dc8a3f364743c13f
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-09-18 21:42:29 -0300

    feat: add entrypoint of the application

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Anglesson\Task\Infrastructure\Adapters\SlimRouteAdapter;
+use Anglesson\Task\Infrastructure\Factories\CreateTaskControllerFactory;
+
+$app = AppFactory::create();
+
+$app->any('/api/task', new SlimRouteAdapter(CreateTaskControllerFactory::create()));
+
+$app->run();
