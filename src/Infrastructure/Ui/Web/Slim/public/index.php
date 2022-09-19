1184ff27681109aa566e35e7f01a070f9edbc700
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-09-19 18:19:32 -0300

    refactor: alter entry point of the application

--- src/Infrastructure/Ui/Web/Slim/public/index.php
+++ src/Infrastructure/Ui/Web/Slim/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '../../../../../../../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use Anglesson\Task\Infrastructure\Adapters\SlimRouteAdapter;
