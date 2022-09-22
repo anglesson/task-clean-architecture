620754575a146288d1c79174e1380d122f70cce4
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-09-22 01:05:20 -0300

    test: add test to entity Task

--- tests/Domain/Entity/TaskTest.php
+++ tests/Domain/Entity/TaskTest.php
@@ -4,17 +4,27 @@ namespace Test\Domain\Entity;
 
 use PHPUnit\Framework\TestCase;
 use Anglesson\Task\Domain\Entity\Task;
+use Anglesson\Task\Domain\Exceptions\DescriptionHasMoreThan50Caracters;
 
-// class TaskTest extends TestCase
-// {
-//     public function testShouldBePopulateEntityTaskWithDescriptionOnly()
-//     {
-//         $task = new Task();
-//         $task->__unserialize([
-//             'id' => 12,
-//             'description'=> 'minha dedscrição'
-//         ]);
-//         $this->assertEquals(is_null($task->description), false);
-//         $this->assertEquals(is_null($task->id), true);
-//     }
-// }
+class TaskTest extends TestCase
+{
+    public function testShouldBePopulateEntityTaskOnlyDescription()
+    {
+        $task = new Task();
+        $task->fill([
+            'id' => 12,
+            'description'=> 'My Description'
+        ]);
+        $this->assertEquals(is_null($task->getDescription()), false);
+        $this->assertEquals(is_null($task->getId()), true);
+    }
+
+    public function testShouldThrowsIfLenghtDescriptionMoreThan50Caracters()
+    {
+        $this->expectException(DescriptionHasMoreThan50Caracters::class);
+        $task = new Task();
+        $task->fill([
+            'description'=> 'My Description has more than fift hundred caracters'
+        ]);
+    }
+}
