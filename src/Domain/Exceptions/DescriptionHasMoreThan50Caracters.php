0c28b4da6f2657ea6bedba194a9ef5e19e45bdc3
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-09-22 01:07:15 -0300

    refactor: ajust code

--- src/Domain/Exceptions/DescriptionHasMoreThan50Caracters.php
+++ src/Domain/Exceptions/DescriptionHasMoreThan50Caracters.php
@@ -7,8 +7,11 @@ use \DomainException;
 
 class DescriptionHasMoreThan50Caracters extends DomainException
 {
-    public function __construct($message = "It is not possible save task with a description more than 50 caracters", $code = 400, Throwable $previous = null)
-    {
+    public function __construct(
+        $message = "It is not possible save task with a description more than 50 caracters",
+        $code = 400,
+        Throwable $previous = null
+    ) {
         parent::__construct($message, $code, $previous);
     }
 }
