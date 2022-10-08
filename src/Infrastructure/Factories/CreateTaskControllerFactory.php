f51148b6bfc49e15225475841c802bd1c4cf29e0
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-08 02:59:49 -0300

    feat: Add DoctrineRepository

--- src/Infrastructure/Factories/CreateTaskControllerFactory.php
+++ src/Infrastructure/Factories/CreateTaskControllerFactory.php
@@ -7,13 +7,14 @@ use App\ToDo\Infrastructure\Utils\RamseyUuidImpl;
 use App\ToDo\Infrastructure\Repositories\InMemory\MockRepository;
 use App\ToDo\Domain\Services\CreateTaskServiceImpl;
 use App\ToDo\Application\Protocols\Http\Controller;
+use App\ToDo\Infrastructure\Repositories\Doctrine\DoctrineRepository;
 
 final class CreateTaskControllerFactory
 {
     public static function create(): Controller
     {
         $ramseyUuid = new RamseyUuidImpl();
-        $repository = new MockRepository($ramseyUuid);
+        $repository = new DoctrineRepository($ramseyUuid);
         $createTaskService = new CreateTaskServiceImpl($repository);
         return new CreateTaskController($createTaskService);
     }
---
+++ src/Infrastructure/Repositories/Doctrine/DoctrineRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\ToDo\Infrastructure\Repositories\Doctrine;
+
+use App\ToDo\Domain\Entity\Task;
+use App\ToDo\Domain\Protocols\CreateTaskRepository;
+use App\ToDo\Infrastructure\Protocols\UuidGenerator;
+use Doctrine\ORM\EntityManager;
+
+class DoctrineRepository implements CreateTaskRepository
+{
+    protected UuidGenerator $uuidGenerator;
+
+    public function __construct(UuidGenerator $uuidGenerator)
+    {
+        $this->uuidGenerator = $uuidGenerator;
+    }
+
+    public function save(Task $task): Task
+    {
+        $task->setId($this->uuidGenerator->generateId());
+        $entityManager = (new EntityManagerCreator())->createEntityManager();
+        $entityManager->persist($task);
+        $entityManager->flush();
+        return $task;
+    }
+}
