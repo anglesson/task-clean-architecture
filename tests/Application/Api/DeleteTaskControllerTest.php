7e99cef816efdadacc2ae60789dfdd47414d2c40
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-14 23:51:09 -0300

    test: Add test task controller

---
+++ tests/Application/Api/DeleteTaskControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Test\Application\Api;
+
+use App\ToDo\Application\Api\DeleteTaskController;
+use App\ToDo\Application\Api\UpdateTaskController;
+use App\ToDo\Domain\Protocols\DeleteTaskService;
+use App\ToDo\Domain\Protocols\UpdateTaskService;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\StreamInterface  as Stream;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\ToDo\Domain\Exceptions\MissingParamsError;
+
+class DeleteTaskControllerTest extends TestCase
+{
+    protected Request $request;
+    protected Response $response;
+
+    protected function setUp(): void
+    {
+        $streamStub = $this->createStub(Stream::class);
+        $requestStub = $this->createStub(Request::class);
+        $responseStub = $this->createStub(Response::class);
+        $responseStub->method('getBody')->willReturn($streamStub);
+
+        $this->request = $requestStub;
+        $this->response = $responseStub;
+    }
+
+    public function testShouldCallServiceWithCorrectIdValue()
+    {
+        $id = 'any_id';
+
+        $this->request
+            ->method('getAttribute')
+            ->with('id')
+            ->willReturn($id);
+
+        $service = $this->createMock(DeleteTaskService::class);
+        $service
+            ->expects($this->once())
+            ->method('delete')
+            ->with($id);
+
+        $controller = new DeleteTaskController($service);
+        $controller->handle($this->request, $this->response);
+    }
+
+    public function testShouldReturn204IfTaskWasDeleteWithSuccess()
+    {
+        $id = 'any_id';
+        $statusCodeExpected = 204;
+
+        $this->request
+            ->method('getAttribute')
+            ->with('id')
+            ->willReturn($id);
+
+        $this->response
+            ->method('getStatusCode')
+            ->willReturn($statusCodeExpected);
+
+        $service = $this->createMock(DeleteTaskService::class);
+        $service
+            ->expects($this->once())
+            ->method('delete')
+            ->with($id);
+
+        $response = (new DeleteTaskController($service))->handle($this->request, $this->response);
+        $this->assertSame($statusCodeExpected, $response->getStatusCode());
+    }
+}
