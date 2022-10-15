d44c8632d701860d72d0f83a6d08ad0e0d8dc0f3
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-15 03:22:58 -0300

    feat: Add ListAllTasksControllerTest class

---
+++ src/Application/Api/ListaAllTaskController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\ToDo\Application\Api;
+
+use App\ToDo\Application\Protocols\Http\Controller;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ListaAllTaskController implements Controller
+{
+
+    public function handle(Request $request, Response $response): Response
+    {
+        // TODO: Implement handle() method.
+    }
+}
