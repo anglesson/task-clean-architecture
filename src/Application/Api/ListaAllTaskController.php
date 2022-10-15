d7ea4a6f565e4c99c92c4852cc1257e41565b51c
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-15 04:07:37 -0300

    feat: add service to controller

--- src/Application/Api/ListaAllTaskController.php
+++ src/Application/Api/ListaAllTaskController.php
@@ -3,14 +3,21 @@
 namespace App\ToDo\Application\Api;
 
 use App\ToDo\Application\Protocols\Http\Controller;
+use App\ToDo\Domain\Protocols\ListAllTasksService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ListaAllTaskController implements Controller
 {
+    public function __construct(
+        private readonly ListAllTasksService $service)
+    {
 
+    }
     public function handle(Request $request, Response $response): Response
     {
+        $result = $this->service->list();
+        $response->getBody()->write(json_encode($result, JSON_PRETTY_PRINT));
         return $response;
     }
 }
