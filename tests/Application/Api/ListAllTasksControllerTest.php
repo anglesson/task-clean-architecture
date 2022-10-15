92dfaee533732dd93562ff2e49a5dbce9f37fb9e
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-15 03:32:05 -0300

    test: add test Should return a response

--- tests/Application/Api/ListAllTasksControllerTest.php
+++ tests/Application/Api/ListAllTasksControllerTest.php
@@ -5,6 +5,8 @@ namespace Application\Api;
 use App\ToDo\Application\Api\ListaAllTaskController;
 use App\ToDo\Application\Protocols\Http\Controller;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class ListAllTasksControllerTest extends TestCase
 {
@@ -12,5 +14,14 @@ class ListAllTasksControllerTest extends TestCase
     {
         $controller = new ListaAllTaskController();
         $this->assertInstanceOf(Controller::class, $controller);
+    }
+
+    public function testShouldReturnAResponse()
+    {
+        $request = $this->createStub(ServerRequestInterface::class);
+        $response = $this->createStub(ResponseInterface::class);
+        $controller = new ListaAllTaskController();
+        $response = $controller->handle($request, $response);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
     }
 }
