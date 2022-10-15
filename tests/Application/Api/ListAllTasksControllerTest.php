cddc38696ef5e716e93dbdfa5eb0fcc039cac8d5
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-15 04:08:29 -0300

    test: add test should call service FindAllTasks

--- tests/Application/Api/ListAllTasksControllerTest.php
+++ tests/Application/Api/ListAllTasksControllerTest.php
@@ -4,24 +4,48 @@ namespace Application\Api;
 
 use App\ToDo\Application\Api\ListaAllTaskController;
 use App\ToDo\Application\Protocols\Http\Controller;
+use App\ToDo\Domain\Protocols\ListAllTasksService;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\StreamInterface as Stream;
 
 class ListAllTasksControllerTest extends TestCase
 {
+    use ProphecyTrait;
+    private Request $request;
+    private Response $response;
+    private Stream $stream;
+
+    public function setUp(): void
+    {
+        $this->request = $this->createStub(Request::class);
+        $this->response = $this->createStub(Response::class);
+        $this->stream = $this->createStub(Stream::class);
+        $this->response->method('getBody')->willReturn($this->stream);
+    }
+
     public function testShouldBeAnInstanceOfController()
     {
-        $controller = new ListaAllTaskController();
+        $service = $this->createStub(ListAllTasksService::class);
+        $controller = new ListaAllTaskController($service);
         $this->assertInstanceOf(Controller::class, $controller);
     }
 
     public function testShouldReturnAResponse()
     {
-        $request = $this->createStub(ServerRequestInterface::class);
-        $response = $this->createStub(ResponseInterface::class);
-        $controller = new ListaAllTaskController();
-        $response = $controller->handle($request, $response);
-        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->response->method('getBody')->willReturnSelf();
+        $service = $this->createStub(ListAllTasksService::class);
+        $controller = new ListaAllTaskController($service);
+        $response = $controller->handle($this->request, $this->response);
+        $this->assertInstanceOf(Response::class, $response);
+    }
+
+    public function testShouldCallServiceFindAllTasks()
+    {
+        $service = $this->prophesize(ListAllTasksService::class);
+        $service->list()->shouldBeCalled();
+        (new ListaAllTaskController($service->reveal()))->handle($this->request, $this->response);
     }
 }
