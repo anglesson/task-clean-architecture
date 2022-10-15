3ea88c7fb21d74fb16535776ad89c64c2a9dbeda
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-14 23:50:49 -0300

    feat: Add delette task controller

---
+++ src/Application/Api/DeleteTaskController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\ToDo\Application\Api;
+
+use App\ToDo\Application\Protocols\Http\Controller;
+use App\ToDo\Domain\Protocols\DeleteTaskService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class DeleteTaskController implements Controller
+{
+    private DeleteTaskService $deleteTaskService;
+
+    public function __construct(DeleteTaskService $deleteTaskService)
+    {
+        $this->deleteTaskService = $deleteTaskService;
+    }
+
+    public function handle(Request $request, Response $response): Response
+    {
+        $idTask = $request->getAttribute('id');
+        $this->deleteTaskService->delete($idTask);
+        return $response;
+    }
+}
