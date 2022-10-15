25b7c173f37e1e0027603f1ca023e3e76ad3651d
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-15 02:43:52 -0300

    test: Add List all tasks service

---
+++ tests/Domain/Services/ListAllTasksServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Test\Domain\Services;
+
+use App\ToDo\Domain\Entity\Task;
+use App\ToDo\Domain\Protocols\ListAllTasksRepository;
+use App\ToDo\Domain\Services\ListAllTasksServiceImpl;
+use PHPUnit\Framework\TestCase;
+use App\ToDo\Domain\Services\FindTaskServiceImpl;
+use App\ToDo\Infrastructure\Utils\RamseyUuidImpl;
+use App\ToDo\Domain\Services\CreateTaskServiceImpl;
+use App\ToDo\Infrastructure\Repositories\InMemory\MockRepository;
+
+class ListAllTasksServiceTest extends TestCase
+{
+    public function makeService(): array
+    {
+        $repository = new MockRepository();
+        $createTaskService = new CreateTaskServiceImpl($repository, new RamseyUuidImpl());
+        $findTaskService = new FindTaskServiceImpl($repository);
+        return array($createTaskService, $findTaskService);
+    }
+
+    public function testShouldListAllTasks()
+    {
+        $repository = $this->createStub(ListAllTasksRepository::class);
+
+        $repository->method('list')->willReturn([
+            $this->createMock(Task::class)->toArray(),
+            $this->createMock(Task::class)->toArray(),
+            $this->createMock(Task::class)->toArray(),
+        ]);
+
+        $service = new ListAllTasksServiceImpl($repository);
+        $tasks = $service->list();
+        $this->assertIsArray($tasks);
+        $this->assertCount(4, $tasks);
+    }
+}
