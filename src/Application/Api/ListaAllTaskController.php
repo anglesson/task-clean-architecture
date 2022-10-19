8ace85f271bcd9d5af1a008dec42b5e905cc6a74
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-10-19 00:57:29 -0300

    feat: add usecase to list all tasks

--- src/Application/Api/ListaAllTaskController.php
+++ src/Application/Api/ListaAllTaskController.php
@@ -2,18 +2,18 @@
 
 namespace App\ToDo\Application\Api;
 
-use App\ToDo\Application\Protocols\Http\Controller;
 use App\ToDo\Domain\Protocols\ListAllTasksService;
+use App\ToDo\Application\Protocols\Http\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ListaAllTaskController implements Controller
 {
     public function __construct(
-        private readonly ListAllTasksService $service)
-    {
-
+        private readonly ListAllTasksService $service
+    ) {
     }
+
     public function handle(Request $request, Response $response): Response
     {
         $result = $this->service->list();
