4c3b3aa8141883ea735334e6d0095bb51fbcec50
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-11-11 23:36:57 -0300

    test: add tests to InputCreateTask

---
+++ tests/Domain/Services/CreateTask/InputCreateTaskTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Test\Domain\Services\CreateTask;
+
+use App\ToDo\Application\DTO\DataTransferObject;
+use App\ToDo\Domain\Services\CreateTask\InputCreateTask;
+use PHPUnit\Framework\TestCase;
+
+class InputCreateTaskTest extends TestCase
+{
+    public function testShouldBeInstaceOfDataTransferObject()
+    {
+        $inputCreateTask = new InputCreateTask();
+        $this->assertInstanceOf(DataTransferObject::class, $inputCreateTask);
+    }
+
+    public function testShouldHaveAtributes()
+    {
+        $inputCreateTask = new InputCreateTask();
+        $this->assertTrue(property_exists($inputCreateTask, 'description'));
+    }
+
+    public function testShoulBeReturnAnArrayWithExpectedKeys()
+    {
+        $inputCreateTask = new InputCreateTask();
+        $this->assertTrue(key_exists('description', $inputCreateTask->toArray()));
+    }
+}
