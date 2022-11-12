19611d533216400b117ba96e90f124de85f11901
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-11-12 01:33:32 -0300

    feat: add output create task class

--- src/Domain/UseCases/CreateTask/OutputCreateTask.php
+++ src/Domain/UseCases/CreateTask/OutputCreateTask.php
@@ -3,7 +3,16 @@
 namespace App\ToDo\Domain\UseCases\CreateTask;
 
 use App\ToDo\Application\DTO\DataTransferObject;
+use App\ToDo\Domain\Entity\Task;
 
 class OutputCreateTask extends DataTransferObject
 {
+    public ?string $id;
+    public ?string $description;
+    public ?bool $finished;
+
+    public static function create(Task $task)
+    {
+        return new self($task->toArray());
+    }
 }
