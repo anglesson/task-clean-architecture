9d24cba214de4d1b095732c0c5b38454002ddc09
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-11-12 20:25:04 -0300

    refactor: remove imports not used

--- tests/Main/Factories/ListAllTaskControllerFactoryTest.php
+++ tests/Main/Factories/ListAllTaskControllerFactoryTest.php
@@ -3,9 +3,7 @@
 namespace Test\Main\Factories;
 
 use App\ToDo\Application\Api\ListAllTaskController;
-use App\ToDo\Application\Api\ReadTaskController;
 use App\ToDo\Main\Factories\ListAllTasksControllerFactory;
-use App\ToDo\Main\Factories\ReadTaskControllerFactory;
 use PHPUnit\Framework\TestCase;
 
 class ListAllTaskControllerFactoryTest extends TestCase
