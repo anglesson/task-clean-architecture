88f2eca6a643c248cdb771a669548a7f753de810
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2022-11-27 18:49:30 -0300

    test: Should be a callable class

---
+++ tests/Main/Adpters/SlimControllerAdapterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Test\Main\Adpters;
+
+use App\ToDo\Application\Protocols\Http\Controller;
+use App\ToDo\Main\Adapters\SlimControllerAdapter;
+use PHPUnit\Framework\TestCase;
+
+class SlimControllerAdapterTest extends TestCase
+{
+    public function testShouldBeACallableClass()
+    {
+        $controllerStub = $this->createStub(Controller::class);
+        $slimControllerAdapter = new SlimControllerAdapter($controllerStub);
+        $this->assertTrue(is_callable($slimControllerAdapter));
+    }
+}
