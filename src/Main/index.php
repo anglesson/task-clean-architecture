1c955c2e0e88a8f0fdc4f5154c2cc5b9f078356b
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2023-03-31 23:43:20 -0300

    feat: change variables name

--- src/Main/index.php
+++ src/Main/index.php
@@ -5,8 +5,7 @@ use App\ToDo\Main\Adapters\Slim\SlimHttpServerAdapter;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-
 $httpServer = new SlimHttpServerAdapter();
-$app = new Router($httpServer);
-$app->init();
+$router = new Router($httpServer);
+$router->init();
 $httpServer->listen();
