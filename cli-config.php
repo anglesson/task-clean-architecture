fe31511c23f5edc7a1239f08e814c0dc36566335
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2023-10-17 15:44:37 -0300

    refactor: remove unused file

--- cli-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-use App\ToDo\Infrastructure\Repositories\Doctrine\EntityManagerCreator;
-
-$entityManager = (new EntityManagerCreator())->createEntityManager();
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
