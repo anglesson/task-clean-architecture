855ab9c25b0a38e391cd2f460474edc1fa4cc3fb
Anglesson Araujo <anglesson@outlook.com>
2024-01-08 22:35:09 -0300

    feat: add home page

---
+++ src/Application/Web/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\ToDo\Application\Web;
+
+use App\ToDo\Application\Protocols\Http\Controller;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class HomeController implements Controller
+{
+    public function handle(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $view = \file_get_contents(__DIR__.'/../Resources/views/home.html');
+        $response->getBody()->write($view);
+        return $response->withHeader('Content-type', 'text/html');
+    }
+}
