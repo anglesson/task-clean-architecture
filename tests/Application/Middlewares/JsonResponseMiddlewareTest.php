9e876601be09628f42b6e9a646b300e01d2510c0
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2024-02-24 11:02:54 -0300

    test: add middleware JsonResponseMiddlewareTest

---
+++ tests/Application/Middlewares/JsonResponseMiddlewareTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Test\Application\Middlewares;
+
+use App\ToDo\Application\Middlewares\JsonResponseMiddleware;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class JsonResponseMiddlewareTest extends TestCase
+{
+    public function testShouldBeAddHeadersToResponse()
+    {
+        $request = $this->createMock(Request::class);
+
+        $response = $this->createMock(Response::class);
+        $response
+            ->expects($this->once())
+            ->method('withHeader')
+            ->with('Content-Type', 'application/json')
+            ->willReturn($this->returnSelf());
+
+        $handler = $this->createMock(RequestHandler::class);
+        $handler->expects($this->once())
+            ->method('handle')
+            ->with($request)
+            ->willReturn($response);
+
+        $jsonResponseMiddleware = new JsonResponseMiddleware();
+
+        $jsonResponseMiddleware->process($request, $handler);
+    }
+}
