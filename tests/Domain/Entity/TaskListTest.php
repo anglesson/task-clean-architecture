3e2a63d875a3eab349aa704b1533186836771475
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2024-04-19 23:45:27 -0300

    test: add test to task list entity

---
+++ tests/Domain/Entity/TaskListTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Test\Domain\Entity;
+
+use App\ToDo\Domain\Entity\Entity;
+use App\ToDo\Domain\Entity\Task;
+use App\ToDo\Domain\Entity\TaskList;
+use PHPUnit\Framework\TestCase;
+
+class TaskListTest extends TestCase
+{
+    public function testShouldExtendsEntityClass()
+    {
+        $list = new TaskList('Inbox');
+        $this->assertInstanceOf(Entity::class, $list);
+    }
+
+    public function testShouldInitializeValues()
+    {
+        $list = new TaskList('Inbox');
+        $this->assertEquals('Inbox', $list->getName());
+        $this->assertEmpty($list->getTasks());
+    }
+
+    public function testShouldAddTaskToAList()
+    {
+        $task = new Task('New Task');
+        $list = new TaskList('Inbox');
+        $list->add($task);
+        $this->assertEquals($task, $list->getTasks()[0]);
+    }
+
+    public function testShouldRemoveTaskFromList()
+    {
+        $task1 = new Task('Task 1');
+        $task2 = new Task('Task 2');
+        $list = new TaskList('Inbox', [$task1, $task2]);
+        $list->remove($task1);
+        $this->assertCount(1, $list->getTasks());
+        $this->assertEquals($task2, $list->getTasks()[0]);
+    }
+
+    public function testShouldRenameList()
+    {
+        $list = new TaskList('Inbox');
+        $list->rename('Next Action');
+        $this->assertEquals('Next Action', $list->getName());
+    }
+}
