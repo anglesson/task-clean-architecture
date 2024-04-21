05b00a4f0e7076736fd604ecb3e78f6f1b230456
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2024-04-21 16:01:14 -0300

    feat: add collection class

---
+++ src/Core/Collection.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\ToDo\Core;
+
+use ArrayObject;
+
+class Collection extends ArrayObject
+{
+    public function isEmpty(): bool
+    {
+        return $this->count() === 0;
+    }
+
+    public function remove(mixed $element): void
+    {
+        if (!$this->isEmpty())
+        {
+            $index = array_search($element, $this->getArrayCopy());
+            $this->offsetUnset($index);
+        }
+    }
+
+    public function get(int $index): mixed
+    {
+        return $this->offsetExists($index) ? $this->offsetGet($index) : null;
+    }
+    public function first(): mixed
+    {
+        return $this->getIterator()->current();
+    }
+
+    public function last(): mixed
+    {
+        $lastIndex = $this->count() - 1;
+        return $this->offsetGet($lastIndex);
+    }
+}
