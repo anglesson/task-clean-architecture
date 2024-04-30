25a919d8e934b2843fc6c8642bc6b3b967be71b1
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2024-04-30 09:16:13 -0300

    test: should save TaskList and return OutputCreateList

--- tests/Domain/UseCases/CreateList/CreateListTest.php
+++ tests/Domain/UseCases/CreateList/CreateListTest.php
@@ -1,6 +1,10 @@
 <?php
 namespace Test\Domain\UseCases\CreateList;
 
+use App\ToDo\Domain\Entity\Task;
+use App\ToDo\Domain\Entity\TaskList;
+use App\ToDo\Domain\Protocols\TaskListRepository;
+use App\ToDo\Domain\Protocols\UuidGenerator;
 use App\ToDo\Domain\UseCases\CreateList\CreateListUseCase;
 use App\ToDo\Domain\UseCases\CreateList\CreateListUseCaseImpl;
 use App\ToDo\Domain\UseCases\CreateList\OutputCreateList;
@@ -8,11 +12,51 @@ use PHPUnit\Framework\TestCase;
 
 class CreateListTest extends TestCase
 {
+    private CreateListUseCaseImpl $sut;
+    private TaskListRepository $mockRepository;
+    public UuidGenerator $mockUuid;
+    protected function setUp(): void
+    {
+        $this->mockUuid = $this->createMock(UuidGenerator::class);
+        $this->mockRepository = $this->createMock(TaskListRepository::class);
+        $this->sut = new CreateListUseCaseImpl($this->mockRepository, $this->mockUuid);
+    }
     public function testShouldCreateANewInstanceOfTaskList()
     {
-        $createList = new CreateListUseCaseImpl();
-        $outputList = $createList->execute();
-        $this->assertInstanceOf(OutputCreateList::class, $outputList);
+        $createList = new CreateListUseCaseImpl($this->mockRepository);
         $this->assertInstanceOf(CreateListUseCase::class, $createList);
+    }
+
+    public function testShouldBeCallRepositoryFunctionWithCorrectValues()
+    {
+        // arrange
+        $expected = new TaskList('any_list_name');
+
+        // assert
+        $this->mockRepository
+            ->expects($this->once())
+            ->method("save")
+            ->with($expected);
+
+        // act
+        $this->sut->execute('any_list_name');
+    }
+
+    public function testShouldBeReturnAOutputCreateTask()
+    {
+        // arrange
+        $taskList = new TaskList('any_list_name');
+        $taskList->add(new Task('Teste'));
+        $expected = OutputCreateList::create($taskList);
+
+        $this->mockRepository
+            ->method("save")
+            ->willReturn($taskList);
+
+        // act
+        $output = $this->sut->execute('any_list_name');
+
+        // assert
+        $this->assertEquals($expected, $output);
     }
 }
