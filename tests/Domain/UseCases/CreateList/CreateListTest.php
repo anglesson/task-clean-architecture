77d6496545ddfeb550e68e3c29f57867706f82e2
Anglesson Araujo <anglesson.araujo@grancursosonline.com.br>
2024-04-30 00:23:10 -0300

    refactor: rename test

--- tests/Domain/UseCases/CreateList/CreateListTest.php
+++ tests/Domain/UseCases/CreateList/CreateListTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class CreateListTest extends TestCase
 {
-    public function testShouldCreateANewList()
+    public function testShouldCreateANewInstanceOfTaskList()
     {
         $createList = new CreateListUseCaseImpl();
         $outputList = $createList->execute();
